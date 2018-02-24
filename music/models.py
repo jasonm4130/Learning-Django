780341c73339690aed863065e7c9b63a0818df9c
jasonm4130 <jasonm4130@gmail.com>
2018-02-24 17:33:34 +1000

    Added Models

--- music/models.py
+++ music/models.py
@@ -7,4 +7,9 @@ class Album(models.Model):
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
     
